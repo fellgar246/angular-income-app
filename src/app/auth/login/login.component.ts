e189ad0d4129688d0ae00d0739e72145e4429892
Luis Felipe Garcia <fellgar246@gmail.com>
2024-09-25 19:49:53 -0600

    update modules and add firestore

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styles: ``
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+
+  loginForm!: FormGroup;
+
+  constructor( private fb: FormBuilder,
+              private authService: AuthService,
+              private router: Router) { }
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: ['',[Validators.required, Validators.email]],
+      password: ['',[Validators.required]]
+    });
+  }
+
+  login() {
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    Swal.fire({
+      title: 'Espere por favor',
+      didOpen: () => {
+        Swal.showLoading();
+      },
+    });
+
+
+    const { email, password } = this.loginForm.value;
+    this.authService.loginUser(email, password)
+      .then( credenciales => {
+        console.log(credenciales);
+        Swal.close();
+        this.router.navigate(['/']);
+      })
+      .catch( err => Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: err.message
+      }));
+  }
 
 }
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styles: ``
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  registroForm!: FormGroup;
 
+  constructor( private fb: FormBuilder,
+              private authService: AuthService,
+              private router: Router){}
+
+  ngOnInit() {
+    this.registroForm = this.fb.group({
+      nombre: ['', Validators.required],
+      correo: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  crearUsuario() {
+    if (this.registroForm.invalid) { return; }
+
+    Swal.fire({
+      title: 'Espere por favor',
+      didOpen: () => {
+        Swal.showLoading();
+      },
+    });
+
+    const { nombre, correo, password } = this.registroForm.value;
+    this.authService.createUser(nombre, correo, password)
+      .then( credenciales => {
+        console.log(credenciales);
+        Swal.close();
+        this.router.navigate(['/']);
+      })
+      .catch( err => Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: err.message
+      }));
+  }
 }
