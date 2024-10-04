cf357581078307dedfe542abce383840a039a664
Luis Felipe Garcia <fellgar246@gmail.com>
2024-10-04 14:27:40 -0600

    update ingresoegreso pipeline

--- src/app/services/ingreso-egreso.service.ts
+++ src/app/services/ingreso-egreso.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { collection, doc, Firestore, setDoc } from '@angular/fire/firestore';
+import { collection, collectionChanges, doc, Firestore, setDoc } from '@angular/fire/firestore';
 import { IngresoEgreso } from '../../models/ingreso-egreso-model';
 import { AuthService } from './auth.service';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -17,5 +18,20 @@ export class IngresoEgresoService {
     const collectionIngresoEgreso = collection(this.firestore, `${uid}/ingreso-egreso/items`);
     const documentRef = doc(collectionIngresoEgreso);
     return setDoc(documentRef, { ...ingresoEgreso })
+  }
+
+  initIngresoEgresoListener(uuid: string) {
+    return collectionChanges(
+      collection(this.firestore, uuid, 'user', 'items')
+    ).pipe(map((items) => {
+      return items.map(_document => {
+        const data = _document.doc.data()
+        console.log(data)
+        return {
+          uuid: _document.doc.id,
+          ...data
+        }
+      })
+    }))
   }
 }
